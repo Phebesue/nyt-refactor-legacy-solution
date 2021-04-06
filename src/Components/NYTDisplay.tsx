c90a543412a2c03360890889e2b5cd1c8e7cb86a
Phebesue <62977906+Phebesue@users.noreply.github.com>
2021-04-05 21:09:13 -0400

    solution

--- src/Components/NYTDisplay.tsx
+++ src/Components/NYTDisplay.tsx
@@ -1,3 +1,6 @@
+
+import {IResult, IKeywords} from "./Interfaces";
+
 import {
   Card,
   CardImg,
@@ -7,8 +10,12 @@ import {
   CardSubtitle,
   Button,
 } from "reactstrap";
-
-const NYTDisplay = () => {
+import { SyntheticEvent } from "react";
+interface IProps{
+  results: IResult[];
+  handlePage:(event:SyntheticEvent, direction: string)=> void;
+}
+const NYTDisplay = ({results,handlePage}: IProps) => {
   return (
     <div style={{ display: "flex", flexWrap: "wrap" }}>
       {results.map((result) => {
@@ -29,7 +36,7 @@ const NYTDisplay = () => {
                 <br />
                 {result.keywords.length > 0 ? " Keywords: " : ""}
               </CardSubtitle>
-              {result.keywords.map((keyword) => (
+              {result.keywords.map((keyword: IKeywords) => (
                 <CardText key={keyword.value}>{keyword.value}</CardText>
               ))}
               <a href={result.web_url}>
